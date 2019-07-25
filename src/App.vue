6eb39569d4f43f2b66723fe75b6c5bf6797076c1
benjaminsinaiko <ben.jamin@outlook.com>
2019-07-25 14:02:04 -0500

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,60 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <QuoteItem v-for="quote in paginatedQuotes" :quote="quote" :key="quote.quote" />
+
+    <!-- <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />-->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import QuoteItem from "./components/QuoteItem.vue";
+import axios from "axios";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    HelloWorld,
+    QuoteItem
+  },
+  data: function() {
+    return {
+      quotes: [],
+      pageNumber: 0,
+      quotesPerPage: 10,
+      displayQuotes: []
+    };
+  },
+  mounted: function() {
+    axios
+      .get(
+        "https://gist.githubusercontent.com/benchprep/dffc3bffa9704626aa8832a3b4de5b27/raw/quotes.json"
+      )
+      .then(res => {
+        this.quotes = res.data;
+      });
+  },
+  computed: {
+    // count num pages
+    pageCount() {
+      const numQuotes = this.quotes.length;
+      return Math.ceil(numQuotes / this.quotesPerPage);
+    },
+    // quotes to display
+    paginatedQuotes() {
+      const first = this.pageNumber * this.quotesPerPage;
+      const last = first + this.quotesPerPage;
+
+      return this.quotes.slice(first, last);
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/QuoteItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>{{quote.context}}</h2>
+    <p>"{{quote.quote}}"</p>
+    <p>--{{quote.source}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "QuotesItem",
+  props: ["quote"],
+  methods: {},
+  compiled: function() {
+    console.log("hello");
+  }
+};
+</script>
